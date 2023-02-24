01147a57518b9115a957091e26a22e2d268093eb
Lantao home PC <lantaook@gmail.com>
2023-02-24 16:33:58 -0500

    build model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, Types, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        },
+        thoughts: [
+            {
+                type: Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+userSchema.virtual("friendCount").get(function () {
+    return `${this.friends.length}`;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
