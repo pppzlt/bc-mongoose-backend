915fb74af6d386e6a22200706a5c0927478d421b
Lantao home PC <lantaook@gmail.com>
2023-02-25 16:15:54 -0500

    finished /api/users/:userId/friends/:friendId routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const { User, Thought } = require("../models");
 
 module.exports = {
@@ -44,11 +45,38 @@ module.exports = {
     },
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => 
+                !user
+                    ? res.status(400).json({ msg: "No user id" })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ msg: "user and related thoughts deleted" }))
+            .catch((err) => res.status(500).json(err));
+    },
+    postFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+            .then((user) => {
+                !user
+                    ? res.status(400).json({ msg: "No user found" })
+                    : res.status(200).json(user);
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
             .then((user) =>
                 !user
-                    ? res.status(400).json({ msg: 'No user id' })
-                    : Thought.deleteMany({ _id: { $in: user.thoughts } }))
-            .then(() => res.json({msg: 'user and related thoughts deleted'}))
-            .catch((err)=> res.status(500).json(err));
+                    ? res.status(400).json({ msg: "No user found" })
+                    : res.status(200).json(user)
+            )
+            .catch((err) => res.json(err));
     },
 };
